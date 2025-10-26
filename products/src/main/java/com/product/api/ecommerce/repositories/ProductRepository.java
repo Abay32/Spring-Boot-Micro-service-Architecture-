8e970273ddd5ee22e2e2ca7fe6bd7032ac7b89b1
Abay Molla Kassa <abay.kassa@tryg.no>
2025-10-26 20:55:27 +0100

    This is a microservice architecture for ecommerce spring boot project

---
+++ products/src/main/java/com/product/api/ecommerce/repositories/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.product.api.ecommerce.repositories;
+
+import com.product.api.ecommerce.models.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @EntityGraph(attributePaths = "category")
+    Page<Product> findByCategoryId(Byte categoryId, Pageable pageable);
+
+    @EntityGraph(attributePaths = "category")
+    @Query("SELECT p FROM Product p")
+    Page<Product> findAllWithCategory(Pageable pageable);
+
+    @EntityGraph(attributePaths = "category")
+    @Query("SELECT COUNT(p) > 0 FROM Product p WHERE p.product_name = :product_name AND p.category.id = :categoryId")
+    boolean existsByNameAndCategoryId(@Param("product_name") String product_name, @Param("categoryId") Byte categoryId);
+}
